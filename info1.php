5cfa8c27e330d05b1ba94d965dec16b121b3e1ab
Mohandes <moosavi.sayyed@outlook.com>
2022-11-27 23:52:53 +0330

    salam

---
+++ info1.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+
+<?php echo $_GET["kfhast"]; ?>
+<br>
+<?php echo $_GET["dskid"]; ?>
+
+
+
+</body>
+</html>
---
+++ js/script.js
@@ -0,0 +1,7 @@
+function mykoon() {
+var kar=document.getElementsByName('karid')
+if(kar.value==""){
+alert("محمد کونی")
+}else{
+}
+}
